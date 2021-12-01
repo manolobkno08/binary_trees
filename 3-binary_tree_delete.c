d649942a0e153703583bdd1a06cabef09437658e
manolobkno08 <manologm10@hotmail.com>
2021-12-01 17:10:13 -0500

    add task3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Delete Binary Tree
+ *
+ * @tree: Get a tree to be deleted
+ * Return: Always Success
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return NULL;
+	else
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
+}
