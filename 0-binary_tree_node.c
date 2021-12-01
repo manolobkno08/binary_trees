52e4574b4c8004623618fa6ac4ca84cd26ebda7f
manolobkno08 <manologm10@hotmail.com>
2021-12-01 15:32:24 -0500

    add task0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,9 +5,20 @@
  *
  * @parent: Pointer to the parent node
  * @value: Value of new node
+ * Return: Always Success
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	int i = 0;
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+
+	return (new_node);
 }
