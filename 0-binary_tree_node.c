9548a51c2c3b3bd80b49119b33db7a805650c735
manolobkno08 <manologm10@hotmail.com>
2021-12-01 14:15:06 -0500

    add structure.h

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Create a node
+ *
+ * @parent: Pointer to the parent node
+ * @value: Value of new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	int i = 0;
+}
