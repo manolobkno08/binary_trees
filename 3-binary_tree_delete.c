99a4a0ac5bcad137dabf61d8a0d5f16cc42c4c92
manolobkno08 <manologm10@hotmail.com>
2021-12-01 17:13:47 -0500

    fixed betty task3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,11 +10,8 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return NULL;
-	else
-	{
-		binary_tree_delete(tree->right);
-		binary_tree_delete(tree->left);
-		free(tree);
-	}
+		return (NULL);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
