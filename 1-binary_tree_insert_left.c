2de2db65d1fb6bd553d9d0f9341d6031bcadd7cb
manolobkno08 <manologm10@hotmail.com>
2021-12-01 16:23:21 -0500

    add task1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Create a node
+ *
+ * @parent: Pointer to the parent node
+ * @value: Value of new node
+ * Return: Always Success
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_left_node;
+	if (parent == NULL)
+		return (NULL);
+
+	new_left_node = binary_tree_node(parent, value);
+	if (new_left_node == NULL)
+		return (NULL);
+
+	new_left_node->left = parent->left;
+
+	if (new_left_node->left != NULL)
+		new_left_node->left->parent = new_left_node;
+	parent->left = new_left_node;
+
+	return (new_left_node);
+}
