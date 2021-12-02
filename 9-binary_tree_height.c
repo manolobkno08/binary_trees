7270763ed015cb047f76547daf974f4be5a7034d
manolobkno08 <manologm10@hotmail.com>
2021-12-01 23:57:43 -0500

    task9 fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
