17095af4c2d8c2ff7509a0a7e850f28bc504a7d5
manolobkno08 <manologm10@hotmail.com>
2021-12-01 23:19:57 -0500

    add task8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - function that goes through a binary Tree
+ *
+ * @tree: Pointer to the left, right branch until root
+ * @func: Func pointer
+ * Return: Always Success
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+	func(tree->n);
+}
